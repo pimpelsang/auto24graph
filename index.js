4291e65dc22bb6cbc29568753611aa1e0eb36f47
pimpelsang <eero.koplimets@gmail.com>
2015-01-17 00:08:22 +0200

    Moved files around

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express'),
 	request = require('request'),
 	app = express();
 
-app.use(express.static(__dirname +'/../'));
+app.use(express.static(__dirname +'/static/'));
 app.get('/auto/', function (req, res) {
 	var url = 'http://www.auto24.ee/kasutatud/nimekiri.php?' + req.param('search');
 	request.get(url).pipe(res);
