cceae2a5c0f6efc6c631b9ada0b527778d7cc762
pimpelsang <eero.koplimets@gmail.com>
2015-01-17 00:18:20 +0200

    Changes for heroku

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var express = require('express'),
 	request = require('request'),
 	app = express();
 
-app.use(express.static(__dirname +'/static/'));
+app.set('port', (process.env.PORT || 1234));
+
+app.use(express.static(__dirname +'/static'));
 app.get('/auto/', function (req, res) {
 	var url = 'http://www.auto24.ee/kasutatud/nimekiri.php?' + req.param('search');
 	request.get(url).pipe(res);
 });
-app.listen(1234);
+app.listen(app.get('port'));
