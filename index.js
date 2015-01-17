eca835d69f9a67b9cf6fe2dc7465c51af4c9b949
pimpelsang <eero.koplimets@gmail.com>
2015-01-17 11:24:05 +0200

    Usage requires password

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var express = require('express'),
 	request = require('request'),
-	app = express();
+	cookieParser = require('cookie-parser');
 
+var app = express();
+app.use(cookieParser());
 app.set('port', (process.env.PORT || 1234));
-
-app.use(express.static(__dirname +'/static'));
 app.get('/auto/', function (req, res) {
 	var url = 'http://www.auto24.ee/kasutatud/nimekiri.php?' + req.param('search');
 	request.get(url).pipe(res);
 });
+app.get('/', function(req, res, next) {
+	if (String(req.cookies.pwd).toLowerCase() !== 'eero') {
+		res.sendfile('./static/login.html');
+	} else {
+		next();
+	}
+});
+
+app.use(express.static(__dirname +'/static'));
 app.listen(app.get('port'));
